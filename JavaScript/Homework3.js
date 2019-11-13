b32513ba45df4b50375779d02706f5c9850571c4
Luc Luba <luc.luba@gmail.com>
2019-11-13 20:15:00 +0100

    Homework3 programming task 2/4

--- JavaScript/Homework3.js
+++ JavaScript/Homework3.js
@@ -58,7 +58,57 @@ const iifeObject = (() => {
 // || 2) Create four function definitions. One for every basic math operations and taking two input parameters. Create one more function. This function is to return calculation object. This object is to have parameters object field that holds two operation parameters inside (x and y) and a function field that points to a function with math operation (defined at the beginning). A function setOperation() that sets the field from previous sentence and a calculate() function that makes calculation and returns its value. 
 // -----------------------------------------------------------------------------
 
-
+const calcObject = (() => {
+    let x, y;
+    let mathOperation = null;
+  
+    const setOperation = (paramX, paramY, operationFn) => {
+      if (typeof paramX === "number" && typeof paramY === "number") {
+        x = paramX;
+        y = paramY;
+      } else {
+        x = null;
+        y = null;
+      }
+  
+      if (typeof operationFn === "function") {
+        mathOperation = operationFn;
+      } else {
+        mathOperation = null;
+      }
+    };
+  
+    const calculate = () => {
+      if (x && y && mathOperation) {
+        return mathOperation(x, y);
+      } else {
+        return "Set parameters first";
+      }
+    };
+  
+    return {
+      setOperation,
+      calculate
+    };
+  })();
+  
+  const add = (x, y) => x + y;
+  const sub = (x, y) => x - y;
+  const multi = (x, y) => x * y;
+  const div = (x, y) => x / y;
+  
+  calcObject.setOperation();
+  console.log(calcObject.calculate());
+  
+  calcObject.setOperation("", "", div);
+  console.log(calcObject.calculate());
+  
+  calcObject.setOperation(4, 4, (x, y) => x + y);
+  console.log(calcObject.calculate());
+  
+  calcObject.setOperation(4, 3, multi);
+  console.log(calcObject.calculate());
+  
 
 // -----------------------------------------------------------------------------
 // || 3) Create an array (by hand) of objects and call sum() function in context of each one of them. Sum function is to come from this object  BaseObject = { X,y, sum: function (){ return this.x+this.y}} 
