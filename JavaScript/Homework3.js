d6ba3583a57d3f0d011a6433c636729ee8e22285
Luc Luba <luc.luba@gmail.com>
2019-11-15 19:21:44 +0100

    Homework3 programming task 3/4

--- JavaScript/Homework3.js
+++ JavaScript/Homework3.js
@@ -108,14 +108,45 @@ const calcObject = (() => {
   
   calcObject.setOperation(4, 3, multi);
   console.log(calcObject.calculate());
-  
+
 
 // -----------------------------------------------------------------------------
 // || 3) Create an array (by hand) of objects and call sum() function in context of each one of them. Sum function is to come from this object  BaseObject = { X,y, sum: function (){ return this.x+this.y}} 
 //Example array: [{x:2,y:3},{x:-1,x:6,{x:0,x:8},…..]
 // -----------------------------------------------------------------------------
 
+const objectsArray = [
+  {
+    x: 1,
+    y: 6
+  },
+  {
+    x: 8,
+    y: 2
+  },
+  {
+    x: 77,
+    y: 9
+  },
+  {
+    x: 13,
+    y: 8
+  },
+  {
+    x: 0,
+    y: 33
+  }
+];
 
+const baseObject = { 
+  x: 0,
+  y: 0,
+
+  sum: function (){ 
+    return this.x + this.y;
+  }};
+
+  objectsArray.forEach( object => console.log(baseObject.sum.call(object)) );
 
 // -----------------------------------------------------------------------------
 // || 4) Given an array of objects, for each object call operation() function in context of next object. If object is last, got back to start of the array for operation function. In loop;
