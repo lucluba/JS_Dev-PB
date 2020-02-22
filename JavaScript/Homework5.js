7b86c7f60faab65a144787185918e1e676545c00
Luc Luba <luc.luba@gmail.com>
2020-02-22 22:50:22 +0100

    Homework5 programming task 5/6

--- JavaScript/Homework5.js
+++ JavaScript/Homework5.js
@@ -134,15 +134,43 @@ class FibonacciSequence {
   function giveLastTwoNum (arr) {
     return [arr[arr.length-1], arr[arr.length-2]];
   }
+}
 
 // -----------------------------------------------------------------------------
 // || 5. Write a code that takes a number and returns a list of its digits.
 // -----------------------------------------------------------------------------
 
+const process = require('process');
+const [,,number] = process.argv;
 
+(function giveDigits ( number=0 ) {
+  console.log(findUniqDigits(convertToDigitsArr(number)));
+})(number);
+
+function convertToDigitsArr (number) {
+  const numString = number.toString();
+  const numArray = changeToNumbers(numString);
+  return [...numArray];
+}
+
+function changeToNumbers ([...str]) {
+  return str.map( letter => Number(letter));
+}
+
+function findUniqDigits (arr) {
+  return arr.reduce( (acc, curr) => {
+    if ( !orContains(acc, curr)) {
+      acc.push(curr);
+    }
+    return acc;
+  }, []);
+}
+
+function orContains (arr, nr) {
+  return arr.includes(nr);
+}
 
 // -----------------------------------------------------------------------------
 // || 6. Write function that translates a text to Pig Latin and back. 
 // -----------------------------------------------------------------------------
-
 
