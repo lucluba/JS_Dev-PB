5c29ac0d1fe7f9c4197fe6cb3349aa2500c9b6b7
Luc Luba <luc.luba@gmail.com>
2020-02-23 07:13:06 +0100

    Homework5 programming task completed

--- JavaScript/Homework5.js
+++ JavaScript/Homework5.js
@@ -174,3 +174,38 @@ function orContains (arr, nr) {
 // || 6. Write function that translates a text to Pig Latin and back. 
 // -----------------------------------------------------------------------------
 
+
+const engText = 'The quick brown fox';
+const pigLatinText = 'heTay uickqay rownbay oxfay';
+
+(function translateToPigLatin (text) {
+  const wordsArray = text.split(' ');
+  const arrInPigLatin = wordsArray.map( word => addAy(moveFirstToEnd(word)) );
+  const textInPigLatin = arrInPigLatin.join(' ');
+  console.log(textInPigLatin);
+})(engText);
+
+(function translateToEng (text) {
+  const wordsArray = text.split(' ');
+  const arrInEng = wordsArray.map( word => moveLastToFront(removeAy(word)) );
+  const textInEng = arrInEng.join(' ');
+  console.log(textInEng);
+})(pigLatinText);
+
+function moveFirstToEnd (word) {
+  const newWord = word.substr(1);
+  return newWord + word[0];
+}
+
+function moveLastToFront (word) {
+  const newWord = word.slice(0, word.length-1);
+  return word[word.length-1] + newWord;
+}
+
+function addAy (word) {
+  return word + 'ay';
+}
+
+function removeAy (word) {
+  return word.slice(0, word.length-2);
+}
