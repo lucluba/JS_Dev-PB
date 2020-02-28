228e3b87e7ce4fd74054e63bf4d943b37ce3c979
Luc Luba <luc.luba@gmail.com>
2020-02-28 23:50:07 +0100

    Homework6 programming tasks completed

--- JavaScript/Homework6.js
+++ JavaScript/Homework6.js
@@ -116,3 +116,78 @@ console.log(finder.findSubstring(first, second));
 // -----------------------------------------------------------------------------
 
 
+
+'use strict';
+const first = [ [2, 1, 3],
+                [-1, 4, 0]];
+const second = [ [1, 3, 2],
+                 [-2, 0, 1],
+                 [5, -3, 2]];
+
+const utils = (() => {
+  function validateMatrices (first, second) {    
+    if (first[0].length === second.length) { return true }
+    else { return false };
+  }
+
+  function calcItem(arr2) {
+    return function reducer (sum, item, idx){
+      return sum += item * arr2[idx];
+    }
+  }
+
+  function iterateColumn (arr, columnNr) {  
+    return arr.map( row => row[columnNr] );
+  }
+
+  return {
+    validateMatrices,
+    calcItem,
+    iterateColumn
+  }
+})();
+
+class MultiRowByColumn {
+  constructor(iterateColumn, calcItem){
+    this.iterateColumn = iterateColumn;
+    this.calcItem = calcItem;
+  }
+
+  multiplyItem(row, arr2, colNr){
+    const column = this.iterateColumn(arr2, colNr);  
+    return row.reduce( this.calcItem(column), 0);
+  }
+}
+
+class MultiplyMatrices {
+  constructor(multiItem, validateMatrices){
+    this.multiItem = multiItem;
+    this.validateMatrices = validateMatrices;
+  }
+
+  multiply(first, second) {
+    if ( this.validateMatrices(first, second) ){
+      const resultMatrice = [[],[]];
+      first.forEach( (row, idx) => {  
+        for (let i=0; i<second[0].length; i++){   
+          resultMatrice[idx].push(this.multiItem(row, second, i));
+        }
+      });
+      return resultMatrice;
+    } else {
+      return 'Matrices cannot be multiplied';
+    }
+  }
+}
+
+const multiRowByColumn = new MultiRowByColumn(utils.iterateColumn, utils.calcItem);
+
+const multiplyMatries = new MultiplyMatrices(
+  multiRowByColumn.multiplyItem.bind(multiRowByColumn),
+  utils.validateMatrices
+)
+const res = multiplyMatries.multiply(first, second);
+
+res.forEach( row => {
+  console.log(row);
+})
